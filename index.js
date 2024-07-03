c2e0f023fd0830f1d1fcc1e4c1ac58a7007cfac7
Syeda Noorain <syeda23bcs10221@ms.sst.scaler.com>
2024-07-03 10:17:21 +0530

    post

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
 let courses = [
     {id: 1, name:"java"},
     {id: 2, name:"javascript"},
-    {id: 3, name:"python"}
+    {id: 3, name:"python"},
 ];
 
 app.get('/courses',(req,res)=>{
     res.json(courses);
 });
+
+app.post('/courses', (req, res) => {
+    console.log(req.body);
+    let singleCourse = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(singleCourse);
+    res.send(courses);
+});
+
+
 
 app.listen(3000,()=>{
     console.log("Server started");
